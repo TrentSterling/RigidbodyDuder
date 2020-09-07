2e0d45ec66788b76d7920964764b1d1a74408065
Trent <trentsterling@gmail.com>
2020-09-07 07:28:39 -0500

    broke some stuff fixed some stuff

--- Assets/RigidbodyController.cs
+++ Assets/RigidbodyController.cs
@@ -16,7 +16,9 @@ public class RigidbodyController : MonoBehaviour
 	Rigidbody body;
 	Vector3 lookDir = Vector3.forward;
 	public float coyoteTime = 0;
-
+	public Collider physCollider;
+	public PhysicMaterial slipperyMaterial;
+	public PhysicMaterial stickyMaterial;
 	void Start()
 	{
 		body = GetComponent<Rigidbody>();
@@ -29,6 +31,11 @@ public class RigidbodyController : MonoBehaviour
 		//get cam rotated input
 		inputVec = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0) * inputVec;
 
+		if (inputVec.magnitude > 1)
+		{
+			inputVec.Normalize();
+		}
+
 		//set rotation direction if input detected
 		if (inputVec.magnitude > 0)
 		{
@@ -37,12 +44,24 @@ public class RigidbodyController : MonoBehaviour
 		}
 
 		//do a jumppy
-		if (Input.GetButton("Jump"))
+		if (Input.GetButtonDown("Jump"))
 		{
 			if (coyoteTime > 0)
 			{
-				body.velocity = Vector3.up * 7.5f;
+				//body.velocity = Vector3.up * 7.5f;
+				physCollider.sharedMaterial = slipperyMaterial;
+				body.drag = 0f;
+				body.AddForce(Vector3.up * -body.velocity.y, ForceMode.VelocityChange);
+				body.AddForce(Vector3.up * 7.5f, ForceMode.VelocityChange);
+				//Vector3 vel = body.velocity;
+				//vel.y = 7.5f;
+				//body.velocity = vel;
+				//body.velocity = Vector3.up * 7.5f;
+				grounded = false;
 			}
+		}
+		if (Input.GetButton("Jump"))
+		{
 			coyoteTime = 0;
 		}
 
@@ -50,24 +69,59 @@ public class RigidbodyController : MonoBehaviour
 	void FixedUpdate()
 	{
 		DetectGround(); //set jump stuff
+		if (grounded)
+		{
 
-		//add stop force
-		Vector3 velocity = body.velocity;
-		velocity.y = 0; //ignore y
-		body.AddForce(-velocity * 0.3f, ForceMode.VelocityChange); //stopping force
+			//add stop force
+			Vector3 velocity = body.velocity;
+			velocity.y = 0; //ignore y
+			body.AddForce(-velocity * 0.2f, ForceMode.VelocityChange); //stopping force
+			//affect drag via input.magnitude
 
-		//add WASD force
-		body.AddForce(inputVec * speed, ForceMode.VelocityChange);
+			if (Input.GetKey(KeyCode.LeftShift))
+			{
+
+				//add WASD force
+				body.AddForce(inputVec * speed * 2, ForceMode.VelocityChange);
+			}
+			else
+			{
+				//add WASD force
+				body.AddForce(inputVec * speed, ForceMode.VelocityChange);
+
+			}
+		}
+		else
+		{
+
+			Vector3 velocity = body.velocity;
+			velocity.y = 0; //ignore y
+			body.AddForce(-velocity * 0.04f, ForceMode.VelocityChange); //stopping force
+			body.AddForce(inputVec * speed * 0.1f, ForceMode.VelocityChange);
+		}
 	}
 	void LateUpdate()
 	{
 		playerModel.rotation = Quaternion.Slerp(playerModel.rotation, Quaternion.LookRotation(lookDir), Time.deltaTime * 5f);
 	}
+	bool grounded = false;
 	public void DetectGround()
 	{
-		if (Physics.Raycast(transform.position + Vector3.up * 0.01f, Vector3.down, 0.02f, groundLayers))
+		//		if (Physics.Raycast(transform.position + Vector3.up * 0.01f, Vector3.down, 0.03f, groundLayers))
+		RaycastHit hit;
+
+		if (Physics.SphereCast(transform.position + Vector3.up * 0.1f, 0.1f, Vector3.down, out hit, 0.2f, groundLayers))
 		{
 			coyoteTime = 0.04f; //tweak this
+			grounded = true;
+			//physCollider.sharedMaterial = stickyMaterial;
+			body.drag = 5f;
+		}
+		else
+		{
+			grounded = false;
+			physCollider.sharedMaterial = slipperyMaterial;
+			body.drag = 0f;
 		}
 	}
 }
