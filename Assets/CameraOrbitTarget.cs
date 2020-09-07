2e74deef0e2e204a817ee292f8a95ac846063866
Trent <trentsterling@gmail.com>
2020-09-07 01:53:26 -0500

    wrote code made prefab

---
+++ Assets/CameraOrbitTarget.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class CameraOrbitTarget : MonoBehaviour
+{
+	public float zoomLevel = 10f;
+	public float xRot = 0;
+	public float yRot = 0;
+
+	public float xRotSmoothed = 0;
+	public float yRotSmoothed = 0;
+
+	public float mouseSensivity = 5f;
+	public float wheelSensivity = 6f;
+
+	public Transform target;
+	public Vector3 offset;
+
+	public SkinnedMeshRenderer playerRenderer;
+
+	void LateUpdate()
+	{
+		zoomLevel -= Input.GetAxis("Mouse ScrollWheel") * wheelSensivity;
+
+		zoomLevel = Mathf.Clamp(zoomLevel, 0, 45);
+
+		float mouseX = Input.GetAxis("Mouse X") * mouseSensivity;
+		float mouseY = -Input.GetAxis("Mouse Y") * mouseSensivity;
+
+		xRot += mouseY;
+		yRot += mouseX;
+
+		xRot = Mathf.Clamp(xRot, -45, 85);
+
+		Cursor.lockState = CursorLockMode.Locked;
+
+		xRotSmoothed = Mathf.Lerp(xRotSmoothed, xRot, Time.smoothDeltaTime * 25f);
+		yRotSmoothed = Mathf.Lerp(yRotSmoothed, yRot, Time.smoothDeltaTime * 25f);
+
+		transform.rotation = Quaternion.Euler(xRotSmoothed, yRotSmoothed, 0);
+
+		transform.position = offset + target.position + transform.forward * -zoomLevel;
+
+		if (zoomLevel < 1.0f)
+		{
+			playerRenderer.enabled = false;
+		}
+		else
+		{
+			playerRenderer.enabled = true;
+		}
+	}
+}
--- Assets/RigidbodyController.cs
+++ Assets/RigidbodyController.cs
@@ -5,19 +5,55 @@ using UnityEngine;
 
 public class RigidbodyController : MonoBehaviour
 {
-	Camera cam;
+	//Camera cam;
+	Vector3 inputVec = Vector3.zero;
+	Rigidbody body;
+	float speed = 2f;
+	public Transform playerModel;
+	Vector3 lookDir = Vector3.forward;
 
-	// Start is called before the first frame update
 	void Start()
 	{
-		//cam = GetComponent<Camera>();
-		cam = Camera.main;
-
+		body = GetComponent<Rigidbody>();
 	}
-
-	// Update is called once per frame
 	void Update()
 	{
+		inputVec.x = Input.GetAxis("Horizontal");
+		inputVec.z = Input.GetAxis("Vertical");
+
+		//get cam rotated input
+		inputVec = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0) * inputVec;
+
+		if (Input.GetButtonDown("Jump"))
+		{
+			body.velocity = Vector3.up * 5f;
+		}
+	}
+	void FixedUpdate()
+	{
+
+		//add stop force
+		Vector3 velocity = body.velocity;
+		velocity.y = 0; //ignore y
+		body.AddForce(-velocity * 0.3f, ForceMode.VelocityChange); //stopping force
+
+		//add WASD force
+		body.AddForce(inputVec * speed, ForceMode.VelocityChange);
+	}
+	void LateUpdate()
+	{
+		/*if (body.velocity.magnitude > 0)
+		{
+			lookDir = body.velocity;
+			lookDir.y = 0;
+			lookDir.Normalize();
+		}*/
+		if (inputVec.magnitude > 0)
+		{
+			lookDir = inputVec;
+			lookDir.Normalize();
+		}
+		playerModel.rotation = Quaternion.Slerp(playerModel.rotation, Quaternion.LookRotation(lookDir), Time.deltaTime * 5f);
 
 	}
 }
