e57ffa01f74bb1ea53189b269e9340324b4f47ab
Trent <trentsterling@gmail.com>
2020-09-07 02:01:57 -0500

    cleanup

--- Assets/RigidbodyController.cs
+++ Assets/RigidbodyController.cs
@@ -6,10 +6,13 @@ using UnityEngine;
 public class RigidbodyController : MonoBehaviour
 {
 	//Camera cam;
+	public Transform playerModel;
+	public float speed = 2f;
+
+	public float stopStrength = 0.3f;
+
 	Vector3 inputVec = Vector3.zero;
 	Rigidbody body;
-	float speed = 2f;
-	public Transform playerModel;
 	Vector3 lookDir = Vector3.forward;
 
 	void Start()
@@ -24,6 +27,14 @@ public class RigidbodyController : MonoBehaviour
 		//get cam rotated input
 		inputVec = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0) * inputVec;
 
+		//set rotation direction if input detected
+		if (inputVec.magnitude > 0)
+		{
+			lookDir = inputVec;
+			lookDir.Normalize();
+		}
+
+		//do a jumppy
 		if (Input.GetButtonDown("Jump"))
 		{
 			body.velocity = Vector3.up * 5f;
@@ -31,7 +42,6 @@ public class RigidbodyController : MonoBehaviour
 	}
 	void FixedUpdate()
 	{
-
 		//add stop force
 		Vector3 velocity = body.velocity;
 		velocity.y = 0; //ignore y
@@ -42,18 +52,6 @@ public class RigidbodyController : MonoBehaviour
 	}
 	void LateUpdate()
 	{
-		/*if (body.velocity.magnitude > 0)
-		{
-			lookDir = body.velocity;
-			lookDir.y = 0;
-			lookDir.Normalize();
-		}*/
-		if (inputVec.magnitude > 0)
-		{
-			lookDir = inputVec;
-			lookDir.Normalize();
-		}
 		playerModel.rotation = Quaternion.Slerp(playerModel.rotation, Quaternion.LookRotation(lookDir), Time.deltaTime * 5f);
-
 	}
 }
