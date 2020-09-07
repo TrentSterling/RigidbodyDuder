2dc50b558e9967003a91689ebb2fe7c4910600ac
Trent <trentsterling@gmail.com>
2020-09-07 01:08:34 -0500

    new unity 3d project

---
+++ Assets/RigidbodyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class RigidbodyController : MonoBehaviour
+{
+	Camera cam;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		//cam = GetComponent<Camera>();
+		cam = Camera.main;
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
